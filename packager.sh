03b875814c105647b2664f67ea263cee37fd3e38
Ray Offiah <ray.offiah@couchbase.com>
2023-08-24 12:13:37 +0100

    Added missing directories to  packager.sh

--- packager.sh
+++ packager.sh
@@ -2,6 +2,8 @@ pyinstaller cb_release_note.py --onefile --collect-all pyfiglet --collect-all al
 mkdir ./package
 cp ./dist/cb_release_note ./package
 cp ./cb_release_notes_config.yaml ./package
+cp ./cb_release_config_schema.yaml ./package
+cp ./.passwords.yaml ./package
 cp -r ./templates ./package
 cd package || exit
 zip -r  ../package.zip .
