c6b70cbc0c5994dca790b11b8565b58c5ce27a5f
Ray Offiah <ray.offiah@couchbase.com>
2025-04-11 17:02:32 +0100

    Removed the password file from the distribution.

--- packager.sh
+++ packager.sh
@@ -1,9 +1,18 @@
-pyinstaller cb_release_note.py --onefile --collect-all pyfiglet --collect-all alive_progress --collect-all grapheme
+pyinstaller cb_release_note.py --onefile \
+--collect-all pyfiglet \
+--collect-all alive_progress \
+--collect-all grapheme \
+--collect-all openai \
+--collect-all numpy
+
 mkdir ./package
 cp ./dist/cb_release_note ./package
 cp ./cb_release_notes_config.yaml ./package
 cp ./cb_release_config_schema.yaml ./package
-cp ./.passwords.yaml ./package
+
+# Don't pack the password file.
+#cp ./.passwords.yaml ./package
+
 cp -r ./templates ./package
 cd package || exit
 zip -r  ../package.zip .
