45f940212e1c487c8850337a2259ade6e09040ed
Ray Offiah <ray.offiah@couchbase.com>
2025-04-14 08:48:16 +0100

    Running pyinstaller with command line options is far easier!

--- packager.sh
+++ packager.sh
@@ -1,5 +1,9 @@
-pyinstaller cb_release_note.spec  --clean
-
+pyinstaller cb_release_note.py --onefile --clean --name cb-release-note \
+--collect-all pyfiglet \
+--collect-all alive_progress \
+--collect-all grapheme \
+--collect-all openai \
+--collect-all numpy \
 
 mkdir ./package
 cp ./dist/cb-release-note ./package
@@ -7,7 +11,7 @@ cp ./cb_release_notes_config.yaml ./package
 cp ./cb_release_config_schema.yaml ./package
 
 # Don't pack the password file.
-# cp ./.passwords.yaml ./package
+cp ./passwords_template.yaml ./package/.passwords.yaml
 
 cp -r ./templates ./package
 cd package || exit
